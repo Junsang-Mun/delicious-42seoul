ff80238f57cc6ad2914737f33eabcfaf5d8703a2
Junsang Mun <mun.js@aol.com>
2023-10-09 22:58:28 +0900

    [feat] main page finished

---
+++ src/routes/+error.svelte
@@ -0,0 +1,4 @@
+<div class="c">
+	<img src="http://http.cat/404.png" alt="404 cat" />
+	<h1 class="title">여기에는 아무것도 없는 것 같네요!</h1>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let userCount = 42;
+	const clickLogin = () => {
+		window.location.href = "https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-e7d3e69db5846309b09f9d93d4d787276306cb3f0f0e8bef5010f33155ac756b&redirect_uri=http%3A%2F%2Flocalhost%3A5173%2Fauth&response_type=code";
+	}
+</script>
+
+<div class="c">
+	<h1 class="title">Delicious 42</h1>
+	<p>{userCount}명과 함께 맛집 찾는 중</p>
+	<br>
+	<btn class="btn primary" on:click={() => clickLogin()}>Login with 42</btn>
+</div>
