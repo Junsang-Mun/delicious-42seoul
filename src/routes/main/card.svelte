b86d3285241c6db9689439c109721038c51b29ae
Junsang Mun <mun.js@aol.com>
2023-10-10 22:48:46 +0900

    [feat] image crop mask

--- src/routes/main/card.svelte
+++ src/routes/main/card.svelte
@@ -5,10 +5,18 @@
 </script>
 
 <div class="card col">
-	<img src="https://picsum.photos/400/300/?random" class="w-100">
+	<img src="https://picsum.photos/500/900/?random" class="w-100">
 	<h4>{title}</h4>
 	<p>{description}</p>
 	<button class="btn primary">정보 보기</button>
 	<button class="btn">길 안내</button>
 	<p>{key}</p>
 </div>
+
+<style>
+	img {
+		width: 400px;
+		height: 300px;
+		object-fit: cover;
+	}
+</style>
